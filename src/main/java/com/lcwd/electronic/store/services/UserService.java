ea0e93f1e08647e7ff9219e488dde82dbee9a43c
Vijay Kumar Kushwaha <vijay77495@gmail.com>
2025-06-14 16:48:30 +0530

    Initial commit for ElectronicStore project

---
+++ src/main/java/com/lcwd/electronic/store/services/UserService.java
@@ -0,0 +1,31 @@
+package com.lcwd.electronic.store.services;
+
+import com.lcwd.electronic.store.dtos.UserDto;
+
+import java.util.List;
+
+public interface UserService {
+    //create
+     UserDto createUser(UserDto userDto);
+
+    //update
+     UserDto updateUser(UserDto userDto, String userId);
+
+    //delete
+     void deleteUser(String userId);
+
+    //get all user
+     List<UserDto> getAllUser();
+
+    //get single user
+     UserDto getUserById(String userId);
+
+    //get single user by email
+     UserDto getUserByEmailByEmail(String email);
+
+    // search user
+     List<UserDto> searchUser(String keyword);
+
+    //other user specific features
+
+}
---
+++ src/main/java/com/lcwd/electronic/store/services/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.lcwd.electronic.store.services.impl;
+
+import com.lcwd.electronic.store.dtos.UserDto;
+import com.lcwd.electronic.store.repositories.UserRepository;
+import com.lcwd.electronic.store.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto userDto, String userId) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public List<UserDto> getAllUser() {
+        return List.of();
+    }
+
+    @Override
+    public UserDto getUserById(String userId) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserByEmailByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> searchUser(String keyword) {
+        return List.of();
+    }
+}
